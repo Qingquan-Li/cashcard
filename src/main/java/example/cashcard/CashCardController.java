d59bd2696258aea9c01726dd8c4e4809def19854
Qingquan Li <liqqisme@gmail.com>
2023-06-27 14:01:25 -0400

    Add Post (create a new cash card)

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,12 +1,16 @@
 package example.cashcard;
 
 import java.util.Optional;
+import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -38,5 +42,23 @@ public class CashCardController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+    
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        // We must provide a Location header with the URI for
+        // where to find the newly created CashCard.
+        // Spring Data's CrudRepository provides methods that support creating, reading, updating,
+        // and deleting data from a data store.
+        // `cashCardRepository.save(newCashCardRequest)` does just as it says: it saves a new CashCard
+        // for us, and returns the saved object with a unique id provided by the database.
+        // We add UriComponentsBuilder ucb as a method argument to this POST handler method and it was
+        // automatically passed in. How so? It was injected from Spring's IoC Container.
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+            .path("/cashcards/{id}")
+            .buildAndExpand(savedCashCard.id())
+            .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import java.net.URI;
+
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
@@ -37,5 +39,33 @@ class CashcardApplicationTests {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isBlank();
+    }
+
+    @Test
+    void shouldCreateANewCashCard() {
+        // The database will create and manage all unique CashCard.id values for us. We should not provide one.
+        CashCard newCashCard = new CashCard(null, 250.00);
+        // We must provide `newCashCard` data for the new CashCard.
+        // We don't expect a CashCard to be returned to us, so we expect a Void response body.
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+        // We expect the HTTP response status code to be 201 CREATED,
+        // which is semantically correct if our API creates a new CashCard from our request.
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        // Send a 201 (Created) response containing a Location header field
+        // that provides an identifier for the primary resource created.
+        // A URL is a type of URI
+        URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+        // We use the Location header's information to fetch the newly created CashCard.
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        // Verify the new CashCard.id is not null, and the newly created CashCard.amount is 250.00,
+        // just as we specified at creation time.
+        DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+        Number id = documentContext.read("$.id");
+        Double amount = documentContext.read("$.amount");
+        assertThat(id).isNotNull();
+        assertThat(amount).isEqualTo(250.00);
     }
 }
